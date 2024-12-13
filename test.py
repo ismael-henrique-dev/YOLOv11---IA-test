ac760c2f3693b9f325a12aabec4e2c728671a3d4
Ismael <ismael.henrique.dev@gmail.com>
2024-12-12 22:13:57 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import cv2
+
+# Carregar o modelo
+model = YOLO("best.pt")
+
+# Caminho para a imagem que você deseja testar
+image_path = "picture.png"
+
+# Realizar a inferência no modelo
+results = model.predict(source=image_path, save=True, save_txt=True)
+
+# Carregar a imagem original para exibir as caixas
+image = cv2.imread(image_path)
+
+# Iterar sobre as detecções
+for result in results:
+    for box in result.boxes.xyxy:  # Coordenadas da caixa delimitadora [x_min, y_min, x_max, y_max]
+        x_min, y_min, x_max, y_max = map(int, box)
+        cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)  # Desenhar a caixa na imagem
+
+# Mostrar a imagem com as caixas
+cv2.imshow("Result", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
